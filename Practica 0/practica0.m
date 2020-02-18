35b8689abbc1d3e22429377f814545afe6ebf71f
Ramsés Castro <ramses-c14@live.com.mx>
2020-02-18 09:57:08 -0600

    Se hacen modificaciones al código

--- Practica 0/practica0.m
+++ Practica 0/practica0.m
@@ -1,15 +1,10 @@
-clear all; %Limpiamos el workspace
-clc; %Limpiamos el command window
-A = imread("imagenes\lena_color_512.tif"); %Leemos la imagen
-A1 = imread("imagenes2\retinaRGB.jpg"); %Leemos la imagen
-A2 = imread("imagenes2\abdomen.png"); %Leemos la imagen
+%%%%%%%%%%%%%%%%%% 1  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Lena = imread("imagenes\lena_color_512.tif"); %Leemos la imagen
+
 %Creamos tres matrices que tendrán los colores primarios de la imagen
-A_R = A; 
-A_G = A;
-A_B = A;
-r = A1(:,:,1); 
-g = A1(:,:,2);
-b = A1(:,:,3);
+A_R = Lena; 
+A_G = Lena;
+A_B = Lena;
 
 %Dejamos en 0 los colores que no nos interesan en cada matriz
 A_R(:,:,2) = 0;
@@ -18,81 +13,126 @@ A_G(:,:,1) = 0;
 A_G(:,:,3) = 0;
 A_B(:,:,1) = 0;
 A_B(:,:,2) = 0;
-B = rgb2gray(A1); %RGB a escala de grises
-C = rgb2hsv(A1); %RGB a hsv
-D = rgb2ycbcr(A1); %RGB a YUV
 
+figure(1);
+subplot(2,2,1);
+imshow(Lena);
+title('Original');
+
+subplot(2,2,2);
+imshow(A_R);
+title('Original solo rojo');
+
+subplot(2,2,3);
+imshow(A_G);
+title('Original solo verde');
+
+subplot(2,2,4);
+imshow(A_B);
+title('Original solo azul');
+
+%%%%%%%%%%%%%%%%%% 2  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Retina = imread("imagenes2\retinaRGB.jpg"); %Leemos la imagen
+
+%Creamos tres matrices que tendrán los colores primarios de la imagen
+r = Retina(:,:,1); 
+g = Retina(:,:,2);
+b = Retina(:,:,3);
+
+B = rgb2gray(Retina); %RGB a escala de grises
+C = rgb2hsv(Retina); %RGB a hsv
 Y = 0.299 * r + 0.587 * g + 0.114 * b;
 U = 0.493*(b-Y);
 V = 0.877*(r-Y);
 E = cat(3,Y,U,V);
 
-A_R(:,:,2) = 0;
-A_R(:,:,3) = 0;
-A_G(:,:,1) = 0;
-A_G(:,:,3) = 0;
-A_B(:,:,1) = 0;
-A_B(:,:,2) = 0;
-
-F = imcrop(A,[0 0 171 512]); %Recortamos la imagen a un tercio
-G = imcrop(A,[313.5 0.5 199 512]); %Recortamos la imagen a un tercio
-H = imcrop(A,[140 410 210 109]); %Recortamos la imagen arbitrariamente
-
-%Impresión de las imágenes
-% image(A2);
-% figure(2);
-% imshow(A);
-% imagesc(A2);
-
-J = imrotate(A,45);
-L = imrotate(A,90);
-M = imrotate(A,180);
-
-figure(3);
-subplot(3,4,1);
-imshow(A);
+figure(2);
+subplot(2,2,1);
+imshow(B);
 title('Original');
 
-subplot(3,4,2);
-imshow(A_R);
-title('Original solo rojo');
-
-subplot(3,4,3);
-imshow(A_G);
-title('Original solo verde');
-
-subplot(3,4,4);
-imshow(A_B);
-title('Original solo azul');
-
-subplot(3,4,5);
+subplot(2,2,2);
 imshow(B);
 title('Imagen a escala de grises');
 
-subplot(3,4,6);
+subplot(2,2,3);
 imshow(C);
 title('Imagen a HSV');
 
-subplot(3,4,7);
-imshow(D);
+subplot(2,2,4);
+imshow(E);
 title('Imagen a YUV');
 
-subplot(3,4,8);
-imshow(E);
-title('Imagen a YUV por fórmula');
+%%%%%%%%%%%%%%%%%% 3  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+F = imcrop(Lena,[0 0 171 512]); %Recortamos la imagen a un tercio
+G = imcrop(Lena,[313.5 0.5 199 512]); %Recortamos la imagen a un tercio
+H = imcrop(Lena,[140 410 210 109]); %Recortamos la imagen arbitrariamente
 
-subplot(3,4,9);
+figure(3);
+subplot(2,2,1);
+imshow(Lena);
+title('Original');
+
+subplot(2,2,2);
 imshow(F);
 title('Imagen recortada un tercio');
 
-subplot(3,4,10);
+subplot(2,2,3);
 imshow(G);
 title('Imagen recortada esquina inferior derecha');
 
-subplot(3,4,11);
+subplot(2,2,4);
 imshow(H);
 title('Imagen recortada arbitrariamente');
 
-subplot(3,4,12);
+%%%%%%%%%%%%%%%%%% 4  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+image(Lena);
+figure(4);
+imshow(Lena);
+
+%%%%%%%%%%%%%%%%%% 5  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Abdomen = imread("imagenes2\abdomen.png"); %Leemos la imagen
+image(Abdomen);
+figure(5);
+imagesc(Abdomen);
+
+%%%%%%%%%%%%%%%%%% 6  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%% 7  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%% 8  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+J = imrotate(Lena,45);
+L = imrotate(Lena,90);
+M = imrotate(Lena,180);
+
+figure(8);
+subplot(2,2,1);
+imshow(Lena);
+title('Original');
+
+subplot(2,2,2);
+imshow(J);
+title('Imagen rotada 45°');
+
+subplot(2,2,3);
 imshow(L);
 title('Imagen rotada 90°');
+
+subplot(2,2,4);
+imshow(M);
+title('Imagen rotada 180°');
+
+%%%%%%%%%%%%%%%%%% 9  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Mri = imread("imagenes2\mri.jpg"); %Leemos la imagen
+Z = rgb2gray(A3); %RGB a escala de grises
+imwrite(Z,'mri.tif');
+
+figure(9);
+subplot(1,2,1);
+imshow(A3);
+title('Imagen mri');
+
+subplot(1,2,2);
+imshow(Z);
+title('Imagen mri blanco y negro');
+%%%%%%%%%%%%%%%%%% 10 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
